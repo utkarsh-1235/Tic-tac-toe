383c07f82492a5fd07cb4e14cdc4ca4c14e039a5
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-08-14 14:17:51 +0530

    update in grid

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -40,7 +40,7 @@ function Grid({numberofCards}){
             }
             <h1 className="turn-highlight">Current turn:{(turn) ? 'O' : 'X'} </h1>
         <div className="grid">
-             {board.map((el, idx)=> <Card key={idx} onPlay={play} player={el} index={idx} />)}
+             {board.map((el, idx)=> <Card gameEnd={winner ? true : false} key={idx} onPlay={play} player={el} index={idx} />)}
         </div>
         </div>
     )
